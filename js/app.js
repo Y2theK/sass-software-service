e2c3854c10b29e07a73b70f2030efeb9dffc2eb4
Y2theK <yeyintkyaw300@gmail.com>
2022-01-23 11:19:38 -0800

    footer, responsive nav and scroll-up done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   //   direction: "vertical",
-  //   loop: true,
+  loop: true,
 
   // If we need pagination
   pagination: {
@@ -14,3 +14,52 @@ const swiper = new Swiper(".swiper", {
     crossFade: true,
   },
 });
+
+//navigation
+const navi = document.querySelector("nav");
+const pos = navi.offsetTop;
+const scrollup = document.querySelector(".scroll-up-item");
+console.log(pos);
+window.onscroll = () => {
+  if (window.pageYOffset >= pos + 100) {
+    navi.classList.add("sticky");
+    scrollup.style.display = "block";
+  } else {
+    navi.classList.remove("sticky");
+    scrollup.style.display = "none";
+  }
+};
+
+//mobile nav
+var x = 0;
+var navicon = document.querySelector(".navbar__bars");
+var bars_icon = document.querySelector(".fa-bars");
+var cross_icon = document.querySelector(".fa-times");
+var mobilenav = document.querySelector(".navbar__menu");
+
+navicon.onclick = () => {
+  if (x == 0) {
+    mobilenav.classList.add("shownav");
+    bars_icon.style.display = "none";
+    cross_icon.style.display = "block";
+
+    x = 1;
+  } else {
+    mobilenav.classList.remove("shownav");
+    bars_icon.style.display = "block";
+    cross_icon.style.display = "none";
+    x = 0;
+  }
+};
+closeNav = () => {
+  mobilenav.classList.remove("shownav");
+  bars_icon.style.display = "block";
+  cross_icon.style.display = "none";
+  x = 0;
+};
+
+//preloader
+const loader = document.querySelector(".preloader");
+setTimeout(function () {
+  loader.style.display = "none";
+}, 000);
